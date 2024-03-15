775e572fb262b69fa31dd17d6183347aaa2309ff
Iván González <cafeteru.dev@gmail.com>
2024-03-15 23:33:01 +0100

    Create first observable

--- src/main/java/io/github/cafeteru/springbootreactor/SpringBootReactorApplication.java
+++ src/main/java/io/github/cafeteru/springbootreactor/SpringBootReactorApplication.java
@@ -1,13 +1,24 @@
 package io.github.cafeteru.springbootreactor;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootReactorApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootReactorApplication.class, args);
+  }
 
+  @Override
+  public void run(String... args) {
+    Flux<String> names = Flux
+      // Crea el observable
+      .just("John", "Jane", "Doe", "Foo", "Bar")
+      // Crea una pipe
+      .doOnNext(System.out::println);
+    names.subscribe();
+  }
 }
