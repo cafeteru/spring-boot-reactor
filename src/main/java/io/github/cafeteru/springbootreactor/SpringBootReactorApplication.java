f591fb682d36fe135a623ed63f52d38ccdeb8118
Iván González <cafeteru.dev@gmail.com>
2024-03-15 23:03:27 +0100

    Create project

---
+++ src/main/java/io/github/cafeteru/springbootreactor/SpringBootReactorApplication.java
@@ -0,0 +1,13 @@
+package io.github.cafeteru.springbootreactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+}
